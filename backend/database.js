212b135e68d8f15cee9d39e239a3023eafcf39d0
MozzaBotAlt <165459795+MozzaBotAlt@users.noreply.github.com>
2025-10-31 22:47:50 +0800

    fix

--- backend/database.js
+++ backend/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -25,3 +25,9 @@ export async function addEmployee(first_name, last_name) {
     const id = result.insertId;
     return getEmployee(id);
 }
+
+const employees = getEmployees()
+console.log(employees)
+
+const employee = getEmployees(1)
+console.log(`The main web dev:` + employee)
