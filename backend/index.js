bf60f5a3f83eba44c75379d0dc3f3fa809ccca9a
Ali Mozzabot I <100562472+AliMBotI@users.noreply.github.com>
2025-11-16 15:46:10 +0800

    hmmz

--- backend/index.js
+++ backend/index.js
@@ -88,10 +88,10 @@ app.post("/api/addemployee", async (req,res) => {
 app.post("/api/github", async (req,res) => {
   const data = req.body;
   console.log(`Github push`, data)
-  res.status(201).send(`Affirmative` data)
+  res.status(201).send(`Affirmative`, data)
 })
 
-app.post('/github/webhook', express.json({type: 'application/json'}), (request, response) => {
+app.post('/', express.json({type: 'application/json'}), (request, response) => {
   response.status(202).send('Accepted');
   const githubEvent = request.headers['x-github-event'];
 
